f728ba8bbfa05bd8d189e4f57d601a7e42ee2330
César <Csar@DESKTOP-4INIV8U>
2024-10-20 18:07:38 -0600

    Agregar archivos de proyecto.

---
+++ ISNP024924_BLOQUE1_Ejercicio3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int suma = 0;
+
+        do
+        {
+            Console.Write("Ingrese un número positivo (0 o negativo para salir): ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                suma += numero;
+            }
+
+        } while (numero > 0);
+
+        Console.WriteLine("La suma total es: " + suma);
+        Console.WriteLine("Programa terminado.");
+    }
+}
